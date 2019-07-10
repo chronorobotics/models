fcd6b2cf22cedb017ec63ccddb8ed8ca6578a53e
Hume2 <teratux.mail@gmail.com>
2019-07-10 12:37:06 +0200

    Unify indentation

--- CPerGaM.cpp
+++ CPerGaM.cpp
@@ -15,7 +15,7 @@ CPerGaM::CPerGaM(int name)
 	gaussian = NULL;
 	storedHistogram = NULL;
 	offset = 0;
-	gain = 1.0/12.0; 
+	gain = 1.0/12.0;
 }
 
 void CPerGaM::init(int iMaxPeriod,int elements,int numActivities)
@@ -31,7 +31,7 @@ void CPerGaM::init(int iMaxPeriod,int elements,int numActivities)
 		gaussian[i].weight = 1.0/12.0/elements;
 		gaussian[i].frequency = maxPeriod;
 	}
-	for (int i=0;i<numElements;i++) storedHistogram[i] = 1.0/numActivities;	
+	for (int i=0;i<numElements;i++) storedHistogram[i] = 1.0/numActivities;
 }
 
 CPerGaM::~CPerGaM()
@@ -46,7 +46,7 @@ int CPerGaM::add(uint32_t time,float state)
 	lastTime = time;
 	storedHistogram[((time%maxPeriod)*numBins/maxPeriod)%numBins] += state;
 	measurements++;
-	return 0; 
+	return 0;
 }
 
 void CPerGaM::update(int order,unsigned int* times,float* signal,int length)
@@ -60,7 +60,7 @@ void CPerGaM::update(int order,unsigned int* times,float* signal,int length)
 	gain = 0;
 	//summarize over all days
 	for (int i=0;i<numBins;i++){
-	       	smooth[i]=0;
+		smooth[i]=0;
 		input[i]=(float)storedHistogram[i]/measurements*numBins;
 		gain+=input[i];
 	}
@@ -68,35 +68,34 @@ void CPerGaM::update(int order,unsigned int* times,float* signal,int length)
 	//smooth it and find a global minimum
 	float sum = 0;
 	int smoothLength = numBins/2;
-	for (int i=numBins-smoothLength;i<numBins;i++) sum+=input[i]; 
+	for (int i=numBins-smoothLength;i<numBins;i++) sum+=input[i];
 	for (int i=numBins;i<numBins*2;i++){
-		 sum = sum - input[(i-smoothLength)%numBins] + input[i%numBins];
-		 smooth[(i-smoothLength/2)%numBins] = sum; 
+		sum = sum - input[(i-smoothLength)%numBins] + input[i%numBins];
+		smooth[(i-smoothLength/2)%numBins] = sum;
 	}
 
 	float minimum = 10.0;
 	for (int i=0;i<numBins;i++){
-		 smooth[i]=smooth[i]/smoothLength;
-		 if (minimum > smooth[i])
-		 {
+		smooth[i]=smooth[i]/smoothLength;
+		if (minimum > smooth[i])
+		{
 			minimum = smooth[i];
 			offset = i;
-		 }
+		}
 	}
 	for (int i=0;i<numBins;i++) refined[i]=input[(i+offset)%numBins];
 	sum = 0;
 	for (int i=0;i<numBins;i++) sum+=input[i];
 
 	//initialize Gaussianu
-
 	delete[] gaussian;
 	int meanDistance = numBins/(numElements+1);
 	gaussian = new SPerGaM[numElements]();
 	for (int i = 0;i<order;i++)
 	{
-	 	gaussian[i].mean = meanDistance*(i+1);
-	 	gaussian[i].weight = 1.0/numElements;
-	 	gaussian[i].sigma = numBins/numElements/2;
+		gaussian[i].mean = meanDistance*(i+1);
+		gaussian[i].weight = 1.0/numElements;
+		gaussian[i].sigma = numBins/numElements/2;
 		gaussian[i].estimate = new float[numBins]();
 	}
 
@@ -115,7 +114,7 @@ void CPerGaM::update(int order,unsigned int* times,float* signal,int length)
 				y[t] = w*exp(-(t-m)*(t-m)/(2*s*s))/(sqrt(2*M_PI)*s);
 				output[t]+=y[t];
 			}
-		}	
+		}
 		//maximization step
 		for (int i = 0;i<order;i++)
 		{
@@ -126,8 +125,8 @@ void CPerGaM::update(int order,unsigned int* times,float* signal,int length)
 			for (int t=0;t<numBins;t++)
 			{
 				y[t]=y[t]/output[t]*refined[t]/sum;
-				m+=t*y[t];	
-				w+=y[t];	
+				m+=t*y[t];
+				w+=y[t];
 			}
 			m = m/w;
 			for (int t=0;t<numBins;t++) s+= (t-m)*(t-m)*y[t];
@@ -151,15 +150,15 @@ void CPerGaM::update(int order,unsigned int* times,float* signal,int length)
 	}
 
 	//print the summary
-//	for (int i=0;i<numBins;i++) printf("SUMAPIC: %s %f %f %f\n",id,output[i]*measurements,refined[i],smooth[i]);
-//	printf("OFFSET %s %i %f %f\n",id,offset,gaussian[0].mean,gaussian[0].sigma);
-		
-//	for (int i=0;i<numBins;i++) printf("SUMAPIC: %s %f %f\n",id,input[i],estimate(i*60));
+	//	for (int i=0;i<numBins;i++) printf("SUMAPIC: %s %f %f %f\n",id,output[i]*measurements,refined[i],smooth[i]);
+	//	printf("OFFSET %s %i %f %f\n",id,offset,gaussian[0].mean,gaussian[0].sigma);
+
+	//	for (int i=0;i<numBins;i++) printf("SUMAPIC: %s %f %f\n",id,input[i],estimate(i*60));
 	delete[] input;
 	delete[] refined;
 	delete[] smooth;
 	delete[] output;
-//	print(false);
+	//	print(false);
 	return;
 }
 
@@ -169,7 +168,7 @@ float CPerGaM::estimate(uint32_t time)
 	timer.start();
 	time = ((time/60+numBins-offset)%numBins);
 	int timi;
-	float probability = 0; 
+	float probability = 0;
 	for (int i=0;i<numElements;i++)
 	{
 		float scale = gaussian[i].weight/(sqrt(2*M_PI)*gaussian[i].sigma);
@@ -179,8 +178,8 @@ float CPerGaM::estimate(uint32_t time)
 	}
 	probability = probability*gain;//*measurements;
 	if (fpclassify(probability) != FP_NORMAL) return 0;
-	/*for (int i = 0;i<phaseShift;i++) probability[i] = signal[signalLength-phaseShift+i]; 
-	for (int i = 0;i<signalLength-phaseShift;i++) probability[i+phaseShift] = signal[i]; 
+	/*for (int i = 0;i<phaseShift;i++) probability[i] = signal[signalLength-phaseShift+i];
+	for (int i = 0;i<signalLength-phaseShift;i++) probability[i+phaseShift] = signal[i];
 	for (int i=0;i<repeats;i++) memcpy(&signal[signalLength*i],probability,signalLength*sizeof(float));*/
 
 	float saturation = 0.01;
@@ -206,10 +205,10 @@ int CPerGaM::importFromArray(double* array,int len)
 	int pos = 0;
 	type = (ETemporalType)array[pos++];
 	if (type != TT_PERGAM) std::cout << "Error loading the model, type mismatch." << std::endl;
-	numElements = array[pos++];  
+	numElements = array[pos++];
 	id = array[pos++];
 	numBins = array[pos++];
-	measurements = array[pos++]; 
+	measurements = array[pos++];
 	memcpy(&firstTime,&array[pos++],sizeof(double));
 	memcpy(&lastTime,&array[pos++],sizeof(double));
 	for (int i = 0;i<numElements&& pos < MAX_TEMPORAL_MODEL_SIZE;i++){
@@ -218,7 +217,7 @@ int CPerGaM::importFromArray(double* array,int len)
 		gaussian[i].weight = array[pos++];
 		gaussian[i].frequency = array[pos++];
 	}
-	for (int i = 0;i<numBins && pos < MAX_TEMPORAL_MODEL_SIZE;i++)storedHistogram[i]=array[pos++]; 
+	for (int i = 0;i<numBins && pos < MAX_TEMPORAL_MODEL_SIZE;i++)storedHistogram[i]=array[pos++];
 
 	if (pos == MAX_TEMPORAL_MODEL_SIZE) std::cout << "Model was not properly saved before." << std::endl;
 	return pos;
