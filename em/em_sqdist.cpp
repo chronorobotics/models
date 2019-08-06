f207f45b49cdbaf4bb226da65c9e4a120c57a974
Hume2 <teratux.mail@gmail.com>
2019-08-06 13:38:32 +0200

    Tune parameters

--- em/em_sqdist.cpp
+++ em/em_sqdist.cpp
@@ -79,7 +79,7 @@ double EMSqdist::maximisation() {
 }
 
 double EMSqdist::time_to_phase(uint32_t time) {
-	float phase = fmodf(time, 86400.0f) / 86400;
+	float phase = fmodf(time, 604800.0f) / 604800;
 	if (phase > 0.5) {
 		phase -= 1;
 	}
