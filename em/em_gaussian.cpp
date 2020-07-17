e7e12983cd6eb19a72f4c303ef24965afe598c9d
Hume2 <teratux.mail@gmail.com>
2020-07-17 13:55:39 +0200

    em/em_gaussian.cpp: fix uninitialised value

--- em/em_gaussian.cpp
+++ em/em_gaussian.cpp
@@ -37,7 +37,7 @@ void EMGaussian::expectation() {
 }
 
 double EMGaussian::maximisation() {
-	double shift;
+	double shift = 0;
 	std::cerr << "Performing maximisation ..." << std::endl;
 
 	for (int i = 0; i < clusters.size(); ++i) {
